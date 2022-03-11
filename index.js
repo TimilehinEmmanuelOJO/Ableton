ae059b310e3e7a50db8f474c83052e3f4d99cbda
Timilehin OJO <timilehin175@gmail.com>
2022-03-11 23:46:45 +0100

    adding media querries

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const myDropdown =()=>{
     document.getElementById("dropdown").classList.toggle("show");
 }
+
+const myMenu =()=>{
+    document.getElementById("menudrop").classList.toggle("open");
+}
