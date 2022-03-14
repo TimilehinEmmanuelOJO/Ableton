be6d68d679859e4245c8c0d469f53ef7171070ae
Timilehin OJO <timilehin175@gmail.com>
2022-03-14 14:22:09 +0100

    top icon

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
+    //created a toggle button to toggle between the normal nav and the "more" dropdown 
 const myDropdown =()=>{
     document.getElementById("dropdown").classList.toggle("show");
 }
 
+
+    //created a toggle button to toggle between the normal nav and the dropdown for the mobile version 
 const myMenu =()=>{
     document.getElementById("menudrop").classList.toggle("open");
 }
